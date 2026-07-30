71d5a2df98a753926eb7186096dbddd405e42c55
Luiz_Vital <luizvital832@gmail.com>
2026-07-30 19:02:31 -0300

    Aula 18

--- src/app/list-render/list-render.ts
+++ src/app/list-render/list-render.ts
@@ -11,23 +11,13 @@ import { List } from '../service/list';
 export class ListRender {
   animalInformations: string = ''
 
-  animals: Animal[] = [
-    {name: 'Turca', type: 'dog', age: 4},
-    {name: 'Tom', type: 'cat', age: 5},
-    {name: 'Frida', type: 'dog', age: 10},
-    {name: 'Bob', type: 'horse', age: 21}
-  ]
-
-  constructor(private listService: List){
+  constructor(private animaisLista: List){
 
   }
 
-  mostrarIdade(animal: Animal){
-    this.animalInformations = `O animal ${animal.name} tem ${animal.age} anos!`
-  }
-
-  removerAnimal(animal: Animal){
-    alert('Animal removido')
-    this.animals = this.listService.remove(this.animals, animal)
+  ngOnInit() {
+    this.animaisLista.getAnimal().subscribe(dados => {
+    console.log(dados);
+    });
   }
 }
--- src/app/service/list.ts
+++ src/app/service/list.ts
@@ -1,9 +1,22 @@
 import { Service } from '@angular/core';
 import { Animal } from '../interfaces/Animal';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
-@Service()
+@Injectable({
+    providedIn: 'root'
+})
+
 export class List {
+
+    constructor(private http: HttpClient){}
+
     remove(animals: Animal[], animal: Animal){
         return animals.filter(a => animal.name !== a.name)
+    }
+
+    getAnimal(){
+        return this.http.get<Animal[]>('http://localhost:3000/animals')
     }
 }
